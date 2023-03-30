a3a4cd9e159b0767fb392e27a1870774bb7bca0b
Liudunlei <Liudunlei@atguigu.com>
2023-03-30 16:09:28 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author liudunlei
+ * @create 2023-03-30 16:05
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
